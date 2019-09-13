dfc92b1885c2b6471324fed287f9b4e6993d2108
Bresenham <gaul@hm.edu>
2019-09-13 12:40:28 +0200

    First Coding Challenge

--- 13092019/DailyProgramming/Daily.cs
+++ 13092019/DailyProgramming/Daily.cs
@@ -2,8 +2,66 @@
 
 namespace DailyProgramming
 {
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
     public class Daily
     {
+        /* https://leetcode.com/problems/add-two-numbers/
+         * You are given two non-empty linked lists representing two non-negative integers.
+         * The digits are stored in reverse order and each of their nodes contain a single digit.
+         * Add the two numbers and return it as a linked list.
+         * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+        */
+
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            Func<ListNode, ListNode, bool> thereAreNextPointers = (lOne, lTwo) => lOne != null && lTwo != null;
+
+            ListNode sumNode = new ListNode(0);
+            ListNode returnNode = sumNode;
+
+            int overhead = 0;
+            while(thereAreNextPointers(l1, l2))
+            {
+
+                int sum = l1.val + l2.val + overhead;
+                if(sum >= 10)
+                {
+                    overhead = 1;
+                    sum %= 10;
+                }
+                sumNode.val = sum;
+
+                if(thereAreNextPointers(l1.next, l2.next))
+                {
+                    sumNode.next = new ListNode(0);
+                    sumNode = sumNode.next;
+                }
+
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+
+            ListNode notNullNode = l1 != null ? l1 : l2;
+
+            while(notNullNode != null)
+            {
+                sumNode.next = new ListNode(notNullNode.val);
+                if(notNullNode.next != null)
+                {
+                    sumNode = sumNode.next;
+                }
+                notNullNode = notNullNode.next;
+            }
+
+            return returnNode;
+        }
+
         static void Main(string[] args)
         {
 
--- 13092019/UnitTest/UnitTest.cs
+++ 13092019/UnitTest/UnitTest.cs
@@ -10,7 +10,84 @@ namespace UnitTest
         [TestMethod]
         public void BasicTest()
         {
+            /* Arrange */
+            ListNode first = new ListNode(2);
+            first.next = new ListNode(4);
+            first.next.next = new ListNode(3);
 
+            ListNode scnd = new ListNode(5);
+            scnd.next = new ListNode(6);
+            scnd.next.next = new ListNode(4);
+
+            /* Act */
+            ListNode result = Daily.AddTwoNumbers(first, scnd);
+
+            /* Assert - This would be 342 + 465 = 807 */
+            Assert.AreEqual(result.val, 7);
+            Assert.AreEqual(result.next.val, 0);
+            Assert.AreEqual(result.next.next.val, 8);
+
+            Assert.AreSame(result.next.next.next, null);
+        }
+
+        [TestMethod]
+        public void UnevenLengthNumberTest()
+        {
+            /* Arrange */
+            ListNode first = new ListNode(1);
+            first.next = new ListNode(2);
+
+            ListNode scnd = new ListNode(5);
+
+            /* Act */
+            ListNode result = Daily.AddTwoNumbers(first, scnd);
+
+            /* Assert - This would be 21 + 5 = 26 */
+            Assert.AreEqual(result.val, 6);
+            Assert.AreEqual(result.next.val, 2);
+        }
+
+        [TestMethod]
+        public void ZeroNumberFirstTest()
+        {
+            /* Arrange */
+            ListNode first = new ListNode(0);
+
+            ListNode scnd = new ListNode(1);
+            scnd.next = new ListNode(1);
+            scnd.next.next = new ListNode(0);
+            scnd.next.next.next = new ListNode(3);
+
+            /* Act */
+            ListNode result = Daily.AddTwoNumbers(first, scnd);
+
+            /* Assert - This would be 0 + 3011 = 3011 */
+            Assert.AreEqual(result.val, 1);
+            Assert.AreEqual(result.next.val, 1);
+            Assert.AreEqual(result.next.next.val, 0);
+            Assert.AreEqual(result.next.next.next.val, 3);
+        }
+
+        [TestMethod]
+        public void ZeroNumberScndTest()
+        {
+            /* Arrange */
+            ListNode first = new ListNode(2);
+            first.next = new ListNode(1);
+            first.next.next = new ListNode(7);
+            first.next.next.next = new ListNode(9);
+
+            ListNode scnd = new ListNode(0);
+
+
+            /* Act */
+            ListNode result = Daily.AddTwoNumbers(first, scnd);
+
+            /* Assert - This would be 9712 + 0 = 9712 */
+            Assert.AreEqual(result.val, 2);
+            Assert.AreEqual(result.next.val, 1);
+            Assert.AreEqual(result.next.next.val, 7);
+            Assert.AreEqual(result.next.next.next.val, 9);
         }
     }
 }
