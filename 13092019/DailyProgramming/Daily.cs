27d9d8ccae7e2f017612b567598924654d4f50dc
Bresenham <gaul@hm.edu>
2019-09-13 10:03:14 +0200

    Initial Commit

---
+++ 13092019/DailyProgramming/Daily.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DailyProgramming
+{
+    public class Daily
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ 13092019/UnitTest/UnitTest.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using DailyProgramming;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+
+        }
+    }
+}
