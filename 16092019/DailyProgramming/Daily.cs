3ae0342d0ba591959ef5065af6b864d9fa7613f5
Bresenham <gaul@hm.edu>
2019-09-19 18:04:32 +0200

    Improved Code very much

--- 16092019/DailyProgramming/Daily.cs
+++ 16092019/DailyProgramming/Daily.cs
@@ -5,46 +5,48 @@ namespace DailyProgramming
     public class Daily
     {
         /*
-         * The string "PAYPALISHIRING" is written in a zigzag pattern
-         * on a given number of rows like this:
+         * The string "PAYPALISHIRING" is written in a zigzag pattern on
+         * a given number of rows like this:
          * 
          * P   A   H   N
          * A P L S I I G
          * Y   I   R
          * 
-         * And then read line by line: "PAHNAPLSIIGYIR.
+         * And then read line by line: "PAHNAPLSIIGYIR"
          * 
-         * Write the code that will take a string and make this conversion
-         * given a number of rows.
-         * 
+         * Write the code that will take a string and make this conversion given a number of rows.
          */ 
-
         public static string Convert(string s, int numRows)
         {
+            int maximumJump = (numRows - 2) * 2 + 2;
             string returnString = "";
-
-            
             for(int r = 0; r < numRows; r++)
             {
                 int i = r;
+                int currentJump = (numRows - r - 2) * 2 + 2;
+                int[] alternatingJumps = { currentJump, maximumJump - currentJump };
+                int alternatingIndex = 0;
                 while (i < s.Length)
                 {
                     returnString += s[i];
                     if(r == 0 || r == numRows - 1)
                     {
-                        i += numRows + 1;
+                        i += maximumJump;
                     } else
                     {
-                        i += (numRows - r - 1) * 2;
+                        i += alternatingJumps[alternatingIndex];
+                        alternatingIndex += 1;
+                        alternatingIndex = alternatingIndex % 2;
                     }
                 }
             }
 
             return returnString;
         }
-        public static void Main(string[] args)
+
+        static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hello World!");
         }
     }
 }
--- 16092019/UnitTest/UnitTests.cs
+++ 16092019/UnitTest/UnitTests.cs
@@ -8,16 +8,68 @@ namespace UnitTest
     public class UnitTests
     {
         [TestMethod]
-        public void TestString1()
+        public void FirstTetst()
         {
             /* Arrange */
-            string test = "PAYPALISHIRING";
+            string s = "PAYPALISHIRING";
 
             /* Act */
-            string returnString = Daily.Convert(test, 3);
+            string returnString = Daily.Convert(s, 3);
 
             /* Assert */
             Assert.AreEqual(returnString, "PAHNAPLSIIGYIR");
+        }
+
+        [TestMethod]
+        public void SecondTest()
+        {
+            /* Arrange */
+            string s = "PAYPALISHIRING";
+
+            /* Act */
+            string returnString = Daily.Convert(s, 4);
+
+            /* Assert */
+            Assert.AreEqual(returnString, "PINALSIGYAHRPI");
+        }
+
+        [TestMethod]
+        public void ThirdTest()
+        {
+            /* Arrange */
+            string s = "PAYPALISHIRING";
+
+            /* Act */
+            string returnString = Daily.Convert(s, 5);
+
+            /* Assert */
+            Assert.AreEqual(returnString, "PHASIYIRPLIGAN");
+        }
+
+        [TestMethod]
+        public void FourthTest()
+        {
+            /* Arrange */
+            string s = "PAYPALISHIRING";
+
+            /* Act */
+            string returnString = Daily.Convert(s, 6);
+
+            /* Assert */
+            Assert.AreEqual(returnString, "PRAIIYHNPSGAIL");
+        }
+
+        [TestMethod]
+        public void FithTest()
+        {
+            /* Arrange */
+            string s = "PAYPALISHIRING";
+
+            /* Act */
+            string returnString = Daily.Convert(s, 2);
+
+            /* Assert */
+            Assert.AreEqual(returnString, "PYAIHRNAPLSIIG");
         }
     }
 }
